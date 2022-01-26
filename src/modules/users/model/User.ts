65c8a68a79b8ac894625cc46862367b54859759c
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-01-26 09:40:50 -0300

    Erro cr

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,11 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: uuidV4;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
 }
-
 export { User };
