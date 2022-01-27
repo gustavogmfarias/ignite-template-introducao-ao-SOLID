09a5a1b205f2e4c7178500d0cc054d6a22213cc2
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-01-27 09:31:03 -0300

    erros variados

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,11 +1,20 @@
 import { v4 as uuidV4 } from "uuid";
+const { v4: uuidv4 } = require("uuid");
 
 class User {
-  id: uuidV4;
+  id: string;
   name: string;
   admin: boolean;
   email: string;
   created_at: Date;
-  updated_at: Date;
+  updated_at?: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+      this.admin = false;
+      this.created_at = new Date();
+    }
+  }
 }
 export { User };
